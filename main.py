9a11c100a7b7d7e5c0f19df758ec10151d2918e5
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-18 15:00:35 +0600

    Introduced list comprehension instead of for loops

--- main.py
+++ main.py
@@ -15,13 +15,20 @@ while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
                                     prompt="What's another state's name?").title()
     if answer_state == "Exit":
-        missing_states = []
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        # missing_states = []
+        # for state in all_states:
+        #     if state not in guessed_states:
+        #         missing_states.append(state)
+        # new_data = pandas.DataFrame(missing_states)
+        # new_data.to_csv("states_to_learn.csv")
+        # break #exits while loop ***
+        #OR....... WE CAN DO ALL THIS IN 1 LINE: USING LIST COMPREHENSION
+        missing_states = [state for state in all_states if state not in guessed_states]
         new_data = pandas.DataFrame(missing_states)
-        new_data.to_csv("states_to_learn.csv")
+        new_data.to_csv("states_to_learn_NOW.csv")
         break #exits while loop ***
+
+
     if answer_state in all_states:
         guessed_states.append(answer_state)
         t = turtle.Turtle()
